095f8dd4a6e89fa1aff140c2c250ddafd164768a
Zdenek Sklenar <arkus14876@seznam.cz>
2015-04-13 16:14:35 +0200

    Created GUI of application

--- inc/gui/mainwindow.h
+++ inc/gui/mainwindow.h
@@ -10,13 +10,42 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
+    QString example;
 private slots:
-    void on_pushButton_clicked();
+
+    void on_Button0_clicked();
+    void on_Button1_clicked();
+    void on_Button2_clicked();
+    void on_Button3_clicked();
+    void on_Button4_clicked();
+    void on_Button5_clicked();
+    void on_Button6_clicked();
+    void on_Button7_clicked();
+    void on_Button8_clicked();
+    void on_Button9_clicked();
+    void on_ButtonDest_clicked();
+
+
+    void on_ButtonBracketL_clicked();
+    void on_ButtonBracketR_clicked();
+
+
+    void on_ButtonAdd_clicked();
+    void on_ButtonSub_clicked();
+
+    void on_ButtonPower_clicked();
+    void on_ButtonRoot_clicked();
+
+    void on_ButtonDiv_clicked();
+    void on_ButtonMul_clicked();
+
+    void on_ButtonLog_clicked();
+
+    void on_ButtonResult_clicked();
 
 private:
     Ui::MainWindow *ui;
--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,8 +1,6 @@
-#include "inc/gui/mainwindow.h"
+#include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "inc/core/evaluator.h"
-
-#include <sstream>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -16,12 +14,106 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_Button0_clicked()
 {
-    Evaluator evaluator;
-    double result = evaluator.evaluate(ui->input->text().toStdString());
-
-    std::ostringstream oss;
-    oss << result;
-    ui->result->setText(QString::fromStdString(oss.str()));
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button0->text());
 }
+
+void MainWindow::on_Button1_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button1->text());
+}
+
+void MainWindow::on_Button2_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button2->text());
+}
+void MainWindow::on_Button3_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button3->text());
+}
+
+void MainWindow::on_Button4_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button4->text());
+}
+
+void MainWindow::on_Button5_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button5->text());
+}
+void MainWindow::on_Button6_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button6->text());
+}
+
+void MainWindow::on_Button7_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button7->text());
+}
+
+void MainWindow::on_Button8_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button8->text());
+}
+void MainWindow::on_Button9_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->Button9->text());
+}
+
+void MainWindow::on_ButtonDest_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonDest->text());
+}
+
+void MainWindow::on_ButtonAdd_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonAdd->text());
+}
+
+void MainWindow::on_ButtonSub_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonSub->text());
+}
+
+void MainWindow::on_ButtonDiv_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonDiv->text());
+}
+
+void MainWindow::on_ButtonMul_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonMul->text());
+}
+
+void MainWindow::on_ButtonBracketL_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonBracketL->text());
+}
+
+void MainWindow::on_ButtonBracketR_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonBracketR->text());
+}
+
+void MainWindow::on_ButtonPower_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonPower->text());
+}
+
+void MainWindow::on_ButtonRoot_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonRoot->text());
+}
+
+void MainWindow::on_ButtonLog_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonLog->text());
+}
+
+void MainWindow::on_ButtonResult_clicked()
+{
+    ui->Example->setText(ui->Example->toPlainText()+ui->ButtonResult->text()+"\n");
+    this->example=ui->Example->toPlainText();
+}
+
